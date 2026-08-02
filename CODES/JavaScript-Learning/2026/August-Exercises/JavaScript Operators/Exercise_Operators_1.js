8ba2d2afa6a44402d9c43fd528b48eb53641ad95
Your Name <yourname@example.com>
2026-08-02 15:02:16 +0100

    JAVASCRIPT OPERATORS ON AUGUST 2ND 2026

---
+++ CODES/JavaScript-Learning/2026/August-Exercises/JavaScript Operators/Exercise_Operators_1.js
@@ -0,0 +1,111 @@
+// Today's date: 2nd August,2026
+// ==========================================
+// QUESTION 1: Addition Operator
+//Write JavaScript code that adds 58 and 26. Store the result inside a variable named total and display it in the console.
+// ==========================================
+let firstNumber = 58;
+let secondNumber = 26;
+let total = firstNumber + secondNumber;
+console.log("The total value of the sum is :", total);
+
+
+// ==========================================
+// QUESTION 2: Subtraction Operator
+//Create two variables named firstNumber and secondNumber. Give them the values 20 and 8. Subtract secondNumber from firstNumber and display the result.
+// ==========================================
+let first_Number = 20;
+let second_Number = 8;
+let difference = first_Number - second_Number;
+console.log("The difference is :", difference);
+
+
+// =========================================
+// QUESTION 3: Multiplication Operator
+// =========================================
+// A packet contains 6 biscuits. Write JavaScript code that calculates the total number of biscuits in 4 packets.
+let biscuitsPerPacket = 6;
+let numberOfPackets = 4;
+
+let totalBiscuits = biscuitsPerPacket * numberOfPackets;
+
+console.log("The total number of biscuits is:", totalBiscuits);
+
+// =========================================
+// QUESTION 4: Division Operator
+// =========================================
+// A mother has 24 sweets and wants to share them equally among 6 children. Use the division operator to calculate how many sweets each child will receive.
+let sweets = 24;
+let children = 6;
+let total_number_of_sweets = 24 / 6;
+console.log("The total number of sweets is :",total_number_of_sweets);
+
+
+// =========================================
+// QUESTION 5: Reminder Operator
+// =========================================
+// Write JavaScript code that uses the remainder operator (%) to find the remainder when 17 is divided by 5.
+// NOTE THE FOLLOWING:
+//let firstNumber = 17;
+//let secondNumber = 5;
+let first_number = 17;
+let second_number = 5;
+let remainder = first_number % second_number;
+console.log("Let the remainder be: ", remainder);
+
+// =========================================
+// QUESTION 6: Reminder Operator
+// =========================================
+//Use the exponent operator (**) to calculate 5 raised to the power of 2. Store the answer inside a variable named result.
+let base = 5;
+let exponent = 2; // or let power = 2;
+
+let result = base ** exponent;
+
+console.log("The result is:", result);
+
+// =========================================
+// QUESTION 7: INCREMENT OPERATOR
+// =========================================
+//Create a variable named count and give it the value 10. Use the increment operator to increase its value by 1. Display the new value.
+let money = 1000;
+let totalMoney= money + 500;
+console.log("The New Value is: ", totalMoney,);
+
+// =========================================
+// QUESTION 8: INCREMENT OPERATOR
+// =========================================
+// Create a variable named score with the value 10. Use the increment operator (++) to increase the score by one.
+let score = 10;
+
+// Use ++ to add 1 to score
+score++; 
+
+console.log("The new score is:", score);
+
+// =========================================
+// QUESTION 9: ADDITION OF TWO STRINGS
+// =========================================
+//Create two variables containing "Lexamitch" and "Enterprises". Join them together with a space and display the complete business name.
+let business = "Lexamitch";
+let companyType = "Enterprises";
+
+let businessName = business + " " + companyType;
+
+console.log("The Business Name is:", businessName);
+
+// =========================================
+// QUESTION 10:
+// =========================================
+//Create three variables containing a string, a number and a Boolean. Use the typeof operator to display the data type of each variable.
+
+// Create three variables containing a string, a number and a Boolean. 
+// Use the typeof operator to display the data type of each variable.
+
+let companyName = "Lexamitch Enterprises";
+let totalProjects = 10;
+let isLearningJS = true;
+
+// Displaying the data types using typeof
+console.log("Data type of companyName:", typeof companyName);
+console.log("Data type of totalProjects:", typeof totalProjects);
+console.log("Data type of isLearningJS:", typeof isLearningJS);
