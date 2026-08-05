1346d5d62567951432e1ef82cf756fcc4ff2681d
Your Name <yourname@example.com>
2026-08-05 03:48:51 +0100

    Exercises on Loops Dated 5th August, 2026

---
+++ CODES/JavaScript-Learning/2026/August-Exercises/JavaScript Loops/Exercise_Loops_1.js
@@ -0,0 +1,68 @@
+// =========================================
+// QUESTION 1: IF STATEMENT
+// =========================================
+//Write an if statement that displays "Welcome" when age is 18 or above.
+let age = 18;
+if (age >=18) {
+    console.log("welcome");
+}
+
+// =========================================
+// QUESTION 2: IF STATEMENT
+// =========================================
+//What will this code display?
+
+let temperature = 35;
+
+if (temperature > 30) {
+  console.log("It is hot");
+}
+
+// =========================================
+// QUESTION 3: IF STATEMENT
+// =========================================
+//Write an if statement that checks whether score is greater than 50.
+let score = 89;
+if (score > 50){
+  console.log("you passed");
+}
+// =========================================
+// QUESTION 4: IF STATEMENT
+// =========================================
+//Write an if...else statement that displays "Adult" when age is 18 or above and "Child" otherwise.
+
+let Age = 20;
+
+if (Age >= 18){
+
+    console.log("Adult");
+
+}else {
+
+    console.log("Child");
+
+} 
+// =========================================
+// QUESTION 5: IF STATEMENT
+// =========================================
+//Write a condition that displays "Correct password" when password is equal to "lex123".
+let password = "lex123";
+
+// Step 2: The door guard checks the password (using THREE equal signs)
+if (password === "lex123") {
+    console.log("Correct password");
+}
+
+// =========================================
+// QUESTION 6: IF STATEMENT
+// =========================================
+//Write an if...else statement that checks whether a number is positive or negative.
+let number = 0;
+
+if (number > 0) {
+    console.log("Positive");
+} else if (number < 0) {
+    console.log("Negative");
+} else {
+    console.log("Zero");
+}
